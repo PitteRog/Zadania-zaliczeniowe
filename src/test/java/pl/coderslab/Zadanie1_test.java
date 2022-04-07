21b123f9546b47b42c861146ed1bff81449c839b
Piotr Rogoziński <91147314+PitteRog@users.noreply.github.com>
2022-04-07 16:50:26 +0200

    First task

---
+++ src/test/java/pl/coderslab/Zadanie1_test.java
@@ -0,0 +1,14 @@
+package pl.coderslab;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/cucumber/features/",
+        plugin = {"pretty","html:out"})
+
+public class Zadanie1_test {
+
+
+}
